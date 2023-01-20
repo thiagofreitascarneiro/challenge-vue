eb7c3853b8ed7773055b568ead91df4748c96266
Thiago <thiagofreitascarneiro@hotmail.com>
2023-01-19 23:02:06 -0300

    clearing unused settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+
   ssr: false,
   head: {
     title: 'challenge-vue',
@@ -22,22 +22,12 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-
-    // SCSS file in the project
     '@/assets/css/main.scss',
-
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/style-resources',
     '@nuxtjs/apollo',
@@ -52,12 +42,10 @@ export default {
     }
   },
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/apollo',
   ],
-  // Apollo configuration
+
   apollo: {
     clientConfigs: {
       default: {
@@ -66,15 +54,10 @@ export default {
     },
   },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
 
   styleResources: {
     scss: [
